7ebacdc8b8309b738aa3a52044d4064ed106b001
simba_mupfu <scmupfururirwa@gmail.com>
2023-07-18 03:04:31 +0200

    initial commit

---
+++ AdaptiveUIDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdaptiveUIDemo
+//
+//  Created by Simbarashe Mupfururirwa on 2023/07/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
